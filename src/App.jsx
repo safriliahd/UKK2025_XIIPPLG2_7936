69b28d4f6a97aff896a1a4a33a9b49675dcf904e
safriliahd <safrilianurhidayah@gmail.com>
2025-02-13 10:51:32 +0700

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from "@mui/material";
+
+
+export default function App() {
+
+  return (
+    <>
+      <Typography variant="h1" sx={{textAlign: 'center'}}>UKK2025_XIIPPLG2_7936</Typography>
+    </>
+  )
+}
+
+
